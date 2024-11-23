380450b951090fc1455641f9769b60a865df9723
NoNormalCreeper <2560359315@qq.com>
2024-11-23 20:44:01 +0800

    WIP: 简易的 PID 控制

--- src/follow_turtle.cpp
+++ src/follow_turtle.cpp
@@ -4,6 +4,17 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "tf2/exceptions.h"
+#include <cmath>
+
+
+// https://figoowen2003.github.io/2021/08/18/%E7%94%A8ROS2%E8%AE%BE%E8%AE%A1%E4%B8%80%E4%B8%AA%E8%B7%9F%E9%9A%8F%E7%9A%84%E5%B0%8F%E4%B9%8C%E9%BE%9F/
+// PID控制参数
+double Kp_linear = 0.5, Ki_linear = 0.0, Kd_linear = 0.05;
+double Kp_angular = 4.0, Ki_angular = 0.0, Kd_angular = 0.2;
+
+// 误差变量
+double prev_error_linear = 0.0, integral_linear = 0.0;
+double prev_error_angular = 0.0, integral_angular = 0.0;
 
 class node: public rclcpp::Node
 {
@@ -23,7 +34,6 @@ private:
     {
 
         geometry_msgs::msg::TransformStamped tf;     
-        geometry_msgs::msg::Twist twi;
         try
         {
             tf = tf_buffer_->lookupTransform(catcher, target, tf2::TimePointZero);
@@ -42,7 +52,26 @@ private:
 
         auto t = tf.transform;
         auto message = geometry_msgs::msg::Twist();
-        auto fixed_dist = 1;
+        auto fixed_dist = 0.5;
+
+
+        // 计算距离误差
+        double distance_error = hypot(t.translation.x, t.translation.y);
+        
+        // 计算角度误差
+        double angle_error = atan2(t.translation.y, t.translation.x);
+
+        // 线性速度PID控制
+        double error_diff_linear = distance_error - prev_error_linear;
+        integral_linear += distance_error;
+        double linear_speed = Kp_linear * distance_error + Ki_linear * integral_linear + Kd_linear * error_diff_linear;
+        prev_error_linear = distance_error;
+
+        // 角速度PID控制
+        double error_diff_angular = angle_error - prev_error_angular;
+        integral_angular += angle_error;
+        double angular_speed = Kp_angular * angle_error + Ki_angular * integral_angular + Kd_angular * error_diff_angular;
+        prev_error_angular = angle_error;
 
         if (hypot(t.translation.x, t.translation.y) < fixed_dist)
         {
@@ -58,8 +87,11 @@ private:
             reach_flag = false;
         }
 
-        message.linear.x = 0.5 * hypot(t.translation.x, t.translation.y);
-        message.angular.z = 1.0 * atan2(t.translation.y, t.translation.x);
+        // message.linear.x = 0.5 * hypot(t.translation.x, t.translation.y);
+        // message.angular.z = 1.0 * atan2(t.translation.y, t.translation.x);
+        message.linear.x = linear_speed;
+        message.angular.z = angular_speed;
+
         RCLCPP_INFO(this->get_logger(), "Publishing: linear.x: '%f', angular.z: '%f'", message.linear.x, message.angular.z);
         publisher_->publish(message);
 

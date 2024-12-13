c751283e75fcd76d65e7576997e14d83ee2089a6
NoNormalCreeper <2560359315@qq.com>
2024-12-14 02:36:29 +0800

    broadcast all the turtles

--- src/tf2_node.cpp
+++ src/tf2_node.cpp
@@ -3,15 +3,18 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/pose.hpp"
 #include "turtlesim/msg/pose.hpp"
+#include <algorithm>
+#include <vector>
+#include <string>
 
 class node: public rclcpp::Node
 {
 private:
     tf2_ros::TransformBroadcaster tfb;
-    std::string turtleName;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
+    std::vector<std::string> turtleNames;
+    std::vector<rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr> subscriptions_;
 
-    void pose_callback(const turtlesim::msg::Pose::SharedPtr msg)
+    void pose_callback(const turtlesim::msg::Pose::SharedPtr msg, const std::string &turtleName)
     {
         geometry_msgs::msg::TransformStamped t;
         t.header.stamp = this->get_clock()->now();
@@ -28,26 +31,36 @@ private:
         t.transform.rotation.w = q.w();
         tfb.sendTransform(t);
     }
+
 public:
-    node(std::string st): Node(st + "_boardcaster"), tfb(this), turtleName(st)
+    node(const std::vector<std::string> &turtles): Node("multi_turtle_broadcaster"), tfb(this), turtleNames(turtles)
     {
-        subscription_ = this->create_subscription<turtlesim::msg::Pose>(
-            turtleName+"/pose", 10, std::bind(&node::pose_callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "Hello, world");
+        for (const auto &turtleName : turtleNames)
+        {
+            auto subscription = this->create_subscription<turtlesim::msg::Pose>(
+                turtleName + "/pose", 10, 
+                [this, turtleName](const turtlesim::msg::Pose::SharedPtr msg) 
+                {
+                    this->pose_callback(msg, turtleName);
+                });
+            subscriptions_.push_back(subscription);
+        }
+        RCLCPP_INFO(this->get_logger(), "Multi-turtle broadcaster node started");
     }
+
     ~node()
     {
-        RCLCPP_INFO(this->get_logger(), "Goodbye, world");
+        RCLCPP_INFO(this->get_logger(), "Multi-turtle broadcaster node shutting down");
     }
 };
 
 class ROS_EVENT_LOOP
 {
 public:
-    ROS_EVENT_LOOP(int argc, char *argv[],std::string st)
+    ROS_EVENT_LOOP(int argc, char *argv[], const std::vector<std::string> &turtleNames)
     {
         rclcpp::init(argc, argv);
-        rclcpp::spin(std::make_shared<node>(st));
+        rclcpp::spin(std::make_shared<node>(turtleNames));
     }
     ~ROS_EVENT_LOOP()
     {
@@ -57,11 +70,18 @@ public:
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2 )
+    if (argc < 2)
     {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: tf turtle_name");
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: tf turtle_name1 [turtle_name2...]");
         return 1;
     }
-    ROS_EVENT_LOOP(argc, argv, argv[1]);
+
+    std::vector<std::string> turtleNames;
+    for (int i = 1; i < argc; ++i)
+    {
+        turtleNames.push_back(argv[i]);
+    }
+
+    ROS_EVENT_LOOP(argc, argv, turtleNames);
     return 0;
 }

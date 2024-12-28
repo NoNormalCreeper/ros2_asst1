b0a9d6189c0bb13ed35de4028d7180eee18c3490
NoNormalCreeper <2560359315@qq.com>
2024-12-28 16:15:23 +0800

    实现固定角度和固定距离

--- src/follow_turtle.cpp
+++ src/follow_turtle.cpp
@@ -19,27 +19,32 @@ double prev_error_angular = 0.0, integral_angular = 0.0;
 class node: public rclcpp::Node
 {
 private:
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher1_, publisher2_;
     rclcpp::TimerBase::SharedPtr timer_;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-    std::string target, catcher;
-    bool reach_flag;
+    std::string target, catcher1, catcher2;
+    bool reach_flag1 = false, reach_flag2 = false;
 
     // Well, these two variables are not used in the code, but they may help in the task
     bool inital_flag;
     geometry_msgs::msg::TransformStamped tf_inital;
 
+    double prev_error_linear1 = 0.0, integral_linear1 = 0.0;
+    double prev_error_linear2 = 0.0, integral_linear2 = 0.0;
+    double prev_error_angular1 = 0.0, integral_angular1 = 0.0;
+    double prev_error_angular2 = 0.0, integral_angular2 = 0.0;
+
     void timer_callback()
     {
-
-        geometry_msgs::msg::TransformStamped tf;     
+        geometry_msgs::msg::TransformStamped tf1, tf2;
         try
         {
-            tf = tf_buffer_->lookupTransform(catcher, target, tf2::TimePointZero);
+            tf1 = tf_buffer_->lookupTransform(catcher1, target, tf2::TimePointZero);
+            tf2 = tf_buffer_->lookupTransform(catcher2, target, tf2::TimePointZero);
         } catch (tf2::TransformException &ex)
         {
-            RCLCPP_ERROR(this->get_logger(), "Could not transform %s to %s: %s", catcher.c_str(), target.c_str(), ex.what());
+            RCLCPP_ERROR(this->get_logger(), "Could not transform: %s", ex.what());
             return;
         }
 
@@ -49,61 +54,89 @@ private:
         // then you can try to make the catcher keep a fixed angle from the target
         // these two tasks are not easy, but you can do it!
 
-        auto t = tf.transform;
-        auto message = geometry_msgs::msg::Twist();
+        auto t1 = tf1.transform;
+        auto t2 = tf2.transform;
+        auto message1 = geometry_msgs::msg::Twist();
+        auto message2 = geometry_msgs::msg::Twist();
         auto fixed_dist = 0.5;
-
+        auto fixed_angle = 10.0;
 
         // 计算距离误差
-        double distance_error = hypot(t.translation.x, t.translation.y);
-        
+        double distance_error1 = hypot(t1.translation.x, t1.translation.y) - fixed_dist;
+        double distance_error2 = hypot(t2.translation.x, t2.translation.y) - fixed_dist;
+
         // 计算角度误差
-        double angle_error = atan2(t.translation.y, t.translation.x);
+        double angle_error1 = atan2(t1.translation.y, t1.translation.x) - fixed_angle;
+        double angle_error2 = atan2(t2.translation.y, t2.translation.x) - fixed_angle;
 
         // 线性速度PID控制
-        double error_diff_linear = distance_error - prev_error_linear;
-        integral_linear += distance_error;
-        double linear_speed = Kp_linear * distance_error + Ki_linear * integral_linear + Kd_linear * error_diff_linear;
-        prev_error_linear = distance_error;
+        double error_diff_linear1 = distance_error1 - prev_error_linear1;
+        integral_linear1 += distance_error1;
+        double linear_speed1 = Kp_linear * distance_error1 + Ki_linear * integral_linear1 + Kd_linear * error_diff_linear1;
+        prev_error_linear1 = distance_error1;
+
+        double error_diff_linear2 = distance_error2 - prev_error_linear2;
+        integral_linear2 += distance_error2;
+        double linear_speed2 = Kp_linear * distance_error2 + Ki_linear * integral_linear2 + Kd_linear * error_diff_linear2;
+        prev_error_linear2 = distance_error2;
 
         // 角速度PID控制
-        double error_diff_angular = angle_error - prev_error_angular;
-        integral_angular += angle_error;
-        double angular_speed = Kp_angular * angle_error + Ki_angular * integral_angular + Kd_angular * error_diff_angular;
-        prev_error_angular = angle_error;
+        double error_diff_angular1 = angle_error1 - prev_error_angular1;
+        integral_angular1 += angle_error1;
+        double angular_speed1 = Kp_angular * angle_error1 + Ki_angular * integral_angular1 + Kd_angular * error_diff_angular1;
+        prev_error_angular1 = angle_error1;
 
-        if (hypot(t.translation.x, t.translation.y) < fixed_dist)
+        double error_diff_angular2 = angle_error2 - prev_error_angular2;
+        integral_angular2 += angle_error2;
+        double angular_speed2 = Kp_angular * angle_error2 + Ki_angular * integral_angular2 + Kd_angular * error_diff_angular2;
+        prev_error_angular2 = angle_error2;
+
+        if (hypot(t1.translation.x, t1.translation.y) < fixed_dist)
         {
-            message.linear.x = 0.0;
-            message.angular.z = 0.0;
-            publisher_->publish(message);
-            if (!reach_flag) RCLCPP_INFO(this->get_logger(), "Reached target");
-            reach_flag = true;
-            return;
+            message1.linear.x = 0.0;
+            message1.angular.z = 0.0;
+            publisher1_->publish(message1);
+            if (!reach_flag1) RCLCPP_INFO(this->get_logger(), "Catcher1 reached target");
+            reach_flag1 = true;
         }
         else
         {
-            reach_flag = false;
+            reach_flag1 = false;
+            message1.linear.x = linear_speed1;
+            message1.angular.z = angular_speed1;
+            publisher1_->publish(message1);
         }
 
-        // message.linear.x = 0.5 * hypot(t.translation.x, t.translation.y);
-        // message.angular.z = 1.0 * atan2(t.translation.y, t.translation.x);
-        message.linear.x = linear_speed;
-        message.angular.z = angular_speed;
+        if (hypot(t2.translation.x, t2.translation.y) < fixed_dist)
+        {
+            message2.linear.x = 0.0;
+            message2.angular.z = 0.0;
+            publisher2_->publish(message2);
+            if (!reach_flag2) RCLCPP_INFO(this->get_logger(), "Catcher2 reached target");
+            reach_flag2 = true;
+        }
+        else
+        {
+            reach_flag2 = false;
+            message2.linear.x = linear_speed2;
+            message2.angular.z = angular_speed2;
+            publisher2_->publish(message2);
+        }
 
-        RCLCPP_INFO(this->get_logger(), "Publishing: linear.x: '%f', angular.z: '%f'", message.linear.x, message.angular.z);
-        publisher_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "Publishing: Catcher1 linear.x: '%f', angular.z: '%f'", message1.linear.x, message1.angular.z);
+        RCLCPP_INFO(this->get_logger(), "Publishing: Catcher2 linear.x: '%f', angular.z: '%f'", message2.linear.x, message2.angular.z);
     }
 
 public:
-    node(std::string target, std::string catcher): Node("follower_" + catcher), target(target), catcher(catcher)
+    node(std::string target, std::string catcher1, std::string catcher2): Node("follower"), target(target), catcher1(catcher1), catcher2(catcher2)
     {
         tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100),
             std::bind(&node::timer_callback, this));
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(catcher + "/cmd_vel", 10);
+        publisher1_ = this->create_publisher<geometry_msgs::msg::Twist>(catcher1 + "/cmd_vel", 10);
+        publisher2_ = this->create_publisher<geometry_msgs::msg::Twist>(catcher2 + "/cmd_vel", 10);
         RCLCPP_INFO(this->get_logger(), "Hello, world");
     }
     ~node()
@@ -118,8 +151,8 @@ public:
     ROS_EVENT_LOOP(int argc, char *argv[], std::string target, std::string catcher1, std::string catcher2)
     {
         rclcpp::init(argc, argv);
-        auto node1 = std::make_shared<node>(target, catcher1);
-        auto node2 = std::make_shared<node>(target, catcher2);
+        auto node1 = std::make_shared<node>(target, catcher1, catcher2);
+        auto node2 = std::make_shared<node>(target, catcher1, catcher2);
         rclcpp::executors::SingleThreadedExecutor executor;
         executor.add_node(node1);
         executor.add_node(node2);
